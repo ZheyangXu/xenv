00c849c54bcddcda9f601dde90511524fc846e4d
ZheyangXu <jishengxzy@hotmail.com>
2022-08-22 21:31:17 +0800

    add oh my zsh

---
+++ zsh/install.sh
@@ -0,0 +1,4 @@
+sh -c "$(curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh)"
+
+git clone git@github.com:zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
